06d4b44fc4ed1322dd9897a2f85532e5c923625d
VirtualJoker <brandenricci@gmail.com>
2022-02-13 00:07:10 -0500

    assign2
    completion

---
+++ assign2/task2/arith.c
@@ -0,0 +1,19 @@
+/*  Example: C program to find area of a circle */
+
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+  float r, a, m, c;
+
+  printf("Enter radius (in mm):\n");
+  scanf("%f", &m);
+
+  r = m/25.4;
+  c = 2 * PI * r;
+  a = PI * r * r;
+
+  printf("Circle's area is %3.2f (sq in).\n", a);
+  printf("Its circumference is %3.2f (in).\n", c);
+}
